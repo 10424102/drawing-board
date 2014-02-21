b7fcd3161802769f070186d7201082c5820133e1
stryabirdsnest <stryabirdsnest@gmail.com>
2014-02-21 11:15:24 +0800

    add UI style

--- projects/DrawingBoard/drawingboard/src/main/java/cn/edu/shu/android/drawingboard/MainActivity.java
+++ projects/DrawingBoard/drawingboard/src/main/java/cn/edu/shu/android/drawingboard/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import cn.edu.shu.android.drawingboard.core.PaintCanvas;
@@ -34,8 +35,9 @@ public class MainActivity extends Activity implements PaintColorPickerDialog.OnC
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
